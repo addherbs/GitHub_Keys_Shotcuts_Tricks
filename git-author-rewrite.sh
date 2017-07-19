66d5f3d3e3d3750228d8ec8d019c1b7e9a73059b
Aditya Pandey <addherbs@gmail.com>
2017-07-18 23:54:36 -0500

    Added how to recommit with different email

---
+++ git-author-rewrite.sh
@@ -0,0 +1,30 @@
+# git clone --bare https://github.com/user/repo.git
+# cd repo.git
+
+
+
+
+
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="[addherbs@gmail.com]"
+CORRECT_NAME="Aditya Pandey"
+CORRECT_EMAIL="addherbs@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+
+# git push --force --tags origin 'refs/heads/*'
+
+# cd ..
+# rm -rf repo.git
